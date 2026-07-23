f05a2428277583afd9c26d8adf369d523a37e357
415-04-jomkhwan <54204@yupparaj.ac.th>
2026-07-23 09:10:57 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,5 +1,7 @@
 import streamlit as sk
 sk.title("🛒 แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7% 💫 ")
 price = sk.number_input("กรอกราคาสินค้า (บาท):", value=0.0)
-sk.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+sk.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท
+net_price = price - vat
 sk.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
+vat = price * 0.07
