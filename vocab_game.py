2a9a957a15c8fc69bc94e19311cfd9a2a0fd1f09
415-04-jomkhwan <54204@yupparaj.ac.th>
2026-09-03 09:01:08 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -35,7 +35,7 @@ def show_result_dialog(ans1, ans2):
         score += 1
     else:
          st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
-       if score == 2:
+    if score == 2:
          st.success("🎉 You win!")
     else:
         st.error("💀 You lose!")
