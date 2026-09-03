11e9d2716d38b228efb4cc8539baeef884fbc15f
415-04-jomkhwan <54204@yupparaj.ac.th>
2026-09-03 08:54:25 +0700

    Create vocab_game.py

---
+++ vocab_game.py
@@ -0,0 +1,88 @@
+import time
+import streamlit as st
+
+st.title("⏱️ เกมเติมศัพท์จับเวลา")
+
+if "ans1_val" not in st.session_state:
+    st.session_state.ans1_val = ""
+if "ans2_val" not in st.session_state:
+    st.session_state.ans2_val = ""
+
+
+def reset_game():
+    st.session_state.ans1_val = "" 
+    st.session_state.ans2_val = "" 
+    st.session_state.start = time.time()  
+    st.session_state.is_ended = False   
+
+
+@st.dialog("📊 สรุปผลการเล่นเกม")
+def show_result_dialog(ans1, ans2):
+    st.balloons()
+    score = 0
+
+    u_ans1 = ans1.strip().lower()
+    u_ans2 = ans2.strip().lower()
+
+    if u_ans1 == "apple":
+        st.success("✅ ข้อ 1: ถูกต้อง")
+        score += 1
+    else:
+        st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
+
+    if u_ans2 == "fish":
+        st.success("✅ ข้อ 2: ถูกต้อง")
+        score += 1
+    else:
+        st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
+       if score == 2:
+        st.success("🎉 You win!")
+    else:
+        st.error("💀 You lose!")
+
+
+st.button("🎮 เริ่มเล่นเกม", on_click=reset_game)
+
+if "start" in st.session_state and not st.session_state.get("is_ended", False):
+    time_left = int(30 - (time.time() - st.session_state.start))
+
+    if time_left > 0:
+        st.error(f"⏳ เหลือเวลา: {time_left} วินาที")
+    else:
+        st.session_state.is_ended = True
+        st.rerun()
+
+st.divider()
+
+ans1 = st.text_input(
+    "ข้อ 1: An `a _ _ l e` a day keeps the doctor away. 🍎",
+    value=st.session_state.ans1_val,
+)
+ans2 = st.text_input(
+    "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
+    value=st.session_state.ans2_val,
+)
+
+st.session_state.ans1_val = ans1
+st.session_state.ans2_val = ans2
+
+
+
+if "start" in st.session_state and not st.session_state.get("is_ended", False):
+    if st.button("📥 ส่งคำตอบ"):
+        st.session_state.is_ended = True
+        st.rerun()
+
+    time.sleep(1)
+
+
+
+    st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
+st.rerun()
+
+if st.session_state.get("is_ended", False):
+    show_result_dialog(ans1, ans2)
+
+st.divider()
+st.write("นางสาว จอมขวัญ อาภัย เลขที่ 4 ม.4/15")
+
