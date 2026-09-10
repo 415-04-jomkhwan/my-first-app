0fd2bc0cc3f7dfc2b126d53717c42d94cc96df7e
415-04-jomkhwan <54204@yupparaj.ac.th>
2026-09-10 09:03:13 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -7,37 +7,58 @@ if "ans1_val" not in st.session_state:
     st.session_state.ans1_val = ""
 if "ans2_val" not in st.session_state:
     st.session_state.ans2_val = ""
+if "ans3_val" not in st.session_state:
+    st.session_state.ans3_val = ""
+if "ans4_val" not in st.session_state:
+    st.session_state.ans4_val = ""
 
 
 def reset_game():
     st.session_state.ans1_val = "" 
     st.session_state.ans2_val = "" 
+    st.session_state.ans3_val = "" 
+    st.session_state.ans4_val = "" 
     st.session_state.start = time.time()  
     st.session_state.is_ended = False   
 
 
 @st.dialog("📊 สรุปผลการเล่นเกม")
-def show_result_dialog(ans1, ans2):
+def show_result_dialog(ans1, ans2, ans3, ans4):
     st.balloons()
     score = 0
 
     u_ans1 = ans1.strip().lower()
     u_ans2 = ans2.strip().lower()
+    u_ans3 = ans3.strip().lower()
+    u_ans4 = ans4.strip().lower()
 
-    if u_ans1 == "apple":
+if u_ans1 == "apple":
         st.success("✅ ข้อ 1: ถูกต้อง")
         score += 1
-    else:
+else:
         st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
 
-    if u_ans2 == "fish":
+if u_ans2 == "fish":
         st.success("✅ ข้อ 2: ถูกต้อง")
         score += 1
-    else:
+else:
          st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
-    if score == 2:
+
+if u_ans1 == "hot":
+        st.success("✅ ข้อ 3: ถูกต้อง")
+        score += 1
+else:
+        st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
+    
+if u_ans1 == "dog":
+        st.success("✅ ข้อ 4: ถูกต้อง")
+        score += 1
+else:
+        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
+    
+ if score == 4:
          st.success("🎉 You win!")
-    else:
+ else:
         st.error("💀 You lose!")
 
 
@@ -46,9 +67,9 @@ st.button("🎮 เริ่มเล่นเกม", on_click=reset_game)
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
     time_left = int(30 - (time.time() - st.session_state.start))
 
-    if time_left > 0:
+if time_left > 0:
         st.error(f"⏳ เหลือเวลา: {time_left} วินาที")
-    else:
+else:
         st.session_state.is_ended = True
         st.rerun()
 
@@ -62,9 +83,20 @@ ans2 = st.text_input(
     "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
     value=st.session_state.ans2_val,
 )
+ans3 = st.text_input(
+    "ข้อ 3: The sun is very `h _t`. ☀️",
+    value=st.session_state.ans3_val,
+)
+ans4 = st.text_input(
+    "ข้อ 4: = The `d _ g`said woof . 🐶",
+    value=st.session_state.ans4_val,
+)
+
 
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
+st.session_state.ans3_val = ans3
+st.session_state.ans4_val = ans4
 
 
 
@@ -81,7 +113,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 st.rerun()
 
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2)
+    show_result_dialog(ans1, ans2, ans3, ans4)
 
 st.divider()
 st.write("นางสาว จอมขวัญ อาภัย เลขที่ 4 ม.4/15")
