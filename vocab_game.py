c4b87f6ccdf4dd30f9a50ef772bf71c5db82e9be
415-04-jomkhwan <54204@yupparaj.ac.th>
2026-09-17 08:54:20 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -27,37 +27,37 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
     st.balloons()
     score = 0
 
-        u_ans1 = ans1.strip().lower()
-        u_ans2 = ans2.strip().lower()
-        u_ans3 = ans3.strip().lower()
-        u_ans4 = ans4.strip().lower()
+    u_ans1 = ans1.strip().lower()
+    u_ans2 = ans2.strip().lower()
+    u_ans3 = ans3.strip().lower()
+    u_ans4 = ans4.strip().lower()
 
-if u_ans1 =="apple":
+if  u_ans1 =="apple":
         st.success("✅ ข้อ 1: ถูกต้อง")
         score += 1
 else:
         st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
 
-if u_ans2 =="fish":
+if  u_ans2 =="fish":
         st.success("✅ ข้อ 2: ถูกต้อง")
         score += 1
 else:
-         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
+        st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
-if u_ans3 =="hot":
+if  u_ans3 =="hot":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
 else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
     
-if u_ans4 =="dog":
+if  u_ans4 =="dog":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
 else:
         st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
     
 if score == 4:
-         st.success("🎉 You win!")
+        st.success("🎉 You win!")
 else:
         st.error("💀 You lose!")
 
