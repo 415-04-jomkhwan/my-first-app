dcd484697405918d7a3661a1ec50a3d917d26a84
415-04-jomkhwan <54204@yupparaj.ac.th>
2026-09-22 16:00:16 +0700

    Update animegame.py

--- animegame.py
+++ animegame.py
@@ -89,6 +89,6 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 
 if st.session_state.get("is_ended", False):
-show_result_dialog(current_answers)
+    show_result_dialog(current_answers)
 
-st.divider()
+    st.divider()
