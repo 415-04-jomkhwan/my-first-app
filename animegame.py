f41ffa32d1f5a46bb9a7b1cbe01b2e097854e04f
415-04-jomkhwan <54204@yupparaj.ac.th>
2026-09-24 08:31:21 +0700

    Update animegame.py

--- animegame.py
+++ animegame.py
@@ -61,7 +61,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
     if time_left > 0:
         st.error(f"⏳ เหลือเวลา: {time_left} วินาที")
-        else:
+    else:
             st.session_state.is_ended = True
             st.rerun()
         
